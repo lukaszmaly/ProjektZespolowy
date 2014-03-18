ef72a6de687e1021820935134168715621bd00cd
madzer34 <madzer34@gmail.com>
2014-03-18 22:36:45 +0100

    Dalsza modyfikacja, brakuje tylko obslugi dla komendy "play"

--- stachecki/klasa_gry.py
+++ stachecki/klasa_gry.py
@@ -1,6 +1,8 @@
 class Game:
 	nick1="";
 	nick2="";
+	nick1hp=20;
+	nick2hp=20;
 	winner = "";
 	count = 0;
 	nicklist=[];
@@ -18,6 +20,7 @@ for x in range (0, 2):
 	if x==0: obiekt.nick1=line;
 	if x==1: obiekt.nick2=line;
 	
+
 #indeks naszej listy
 i=0;
 obiekt.nick1 = obiekt.nick1[:-1];
@@ -35,14 +38,24 @@ for y in range (3,ogr):
 	else:
 	#dzielimy funkcja split dana linie
 		list=line.split (" ");	
-		#CZEMU TO NIE DZIALA !!!!
 		if("win" in list[0]):
 			obiekt.winner=list[1];
 		elif (obiekt.nick1 in list[1]) or (obiekt.nick2 in list[1]):
 			obiekt.nicklist.append(list[1][:-1]);
-		elif ("play:" in list[0] or "tap:" in list[0]):			
-			if("mana" in list[1]):
-				obiekt.manalist.append(list[1][:-1]);
+		#jeszcze nie wiem, co blok mialby robic przy wizualizacji
+		elif ("block:" in list[0]):
+			newlist=line.split(":");
+			obiekt.actionlist.append(line);
+		elif ("tap:" in list[0] or "counter:" in list[0] or "dead:"in list[0] or "skill:" in list[0]):		
+			if ("target" in line):
+				obiekt.actionlist.append(line);
+			elif("mana" in list[1]):
+				slowo=list[1];
+				if (slowo[len(slowo)-1]=="\n"):
+					slowo=slowo[:-1];
+					obiekt.manalist.append(slowo);
+				else:
+					obiekt.manalist.append(list[1]);
 				obiekt.actionlist.append(line);
 			else: 
 				dolacz="";
@@ -50,23 +63,27 @@ for y in range (3,ogr):
 					dolacz=dolacz+" "+list[j];
 				obiekt.cardlist.append(dolacz);
 				obiekt.actionlist.append(line);
-		elif("HP" in list[0]):
+		elif("HP_"+obiekt.nick1 in list[0]):
+			nick1hp = list[1];
+		elif ("HP_"+obiekt.nick2 in list[0]):
+			nick2hp=list[1];
+		#samo play, z targetem lub 1/1 cokolwiek to znaczy
+		elif ("play:" in list[0]):
 			continue;
-
-		elif("counter:" in list[0] or "dead:"in list[0] or "skill:" in list[0] or "block:" in list[0]):
-			obiekt.actionlist.append(line);
+		
 		else:
 			obiekt.actionlist.append(line);
-			
+	obiekt.count = obiekt.count + 1;
 #zamykamy plik
 fo.close();
 
-for k in range (0,len(obiekt.nicklist)):
-	print(obiekt.nicklist[k]);
-
-for k in range (0,len(obiekt.manalist)):
-	print(obiekt.manalist[k]);
-	
+#nie dzialaja nazwy kart i targety
 for k in range (0,len(obiekt.cardlist)):
 	print(obiekt.cardlist[k]);
 
+#nicki dzialaja
+#hp dzialaja
+#mana dziala
+
+#play, play target i block 
+	
