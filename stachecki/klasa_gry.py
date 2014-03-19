26de54ee7ef228eedc1fe12456da53aa7a8c9a3a
madzer34 <madzer34@gmail.com>
2014-03-19 10:23:20 +0100

    Prawidlowe wczytywanie wszystkich danych

--- stachecki/klasa_gry.py
+++ stachecki/klasa_gry.py
@@ -1,3 +1,5 @@
+#obsluga z ladowaniem wszystkiego jak leci
+#a teraz bedziemy ladowac to, czego nie ma
 class Game:
 	nick1="";
 	nick2="";
@@ -24,10 +26,14 @@ for x in range (0, 2):
 #indeks naszej listy
 i=0;
 obiekt.nick1 = obiekt.nick1[:-1];
-obiekt.nick2 = obiekt.nick2[:-2];
+obiekt.nick2 = obiekt.nick2[:-1];
+if (obiekt.nick2[len(obiekt.nick2)-1]=="\n"):
+	obiekt.nick2=obiekt.nick2[:-1];
+if (obiekt.nick1[len(obiekt.nick1)-1]=="\n"):
+	obiekt.nick1=obiekt.nick1[:-1];
 
 #ogr to numer ostatniej linijki pliku
-ogr = int( int(obiekt.count)+3)*100;
+ogr = int( int(obiekt.count)+3)*1000;
 #wczytujemy przebieg gry
 for y in range (3,ogr):
 	line = fo.readline();
@@ -37,7 +43,9 @@ for y in range (3,ogr):
 	elif not line:break;
 	else:
 	#dzielimy funkcja split dana linie
-		list=line.split (" ");	
+		list=line.split (" ");
+	#wczytujemy akcje
+		obiekt.actionlist.append(line);
 		if("win" in list[0]):
 			obiekt.winner=list[1];
 		elif (obiekt.nick1 in list[1]) or (obiekt.nick2 in list[1]):
@@ -45,45 +53,66 @@ for y in range (3,ogr):
 		#jeszcze nie wiem, co blok mialby robic przy wizualizacji
 		elif ("block:" in list[0]):
 			newlist=line.split(":");
-			obiekt.actionlist.append(line);
-		elif ("tap:" in list[0] or "counter:" in list[0] or "dead:"in list[0] or "skill:" in list[0]):		
-			if ("target" in line):
-				obiekt.actionlist.append(line);
-			elif("mana" in list[1]):
+		elif ("tap:" in list[0] or "counter:" in list[0] or "dead:"in list[0] or "skill:" in list[0] or "atack:" in list[0]):		
+			if("mana" in list[1]):
 				slowo=list[1];
 				if (slowo[len(slowo)-1]=="\n"):
 					slowo=slowo[:-1];
 					obiekt.manalist.append(slowo);
 				else:
 					obiekt.manalist.append(list[1]);
-				obiekt.actionlist.append(line);
+			
 			else: 
 				dolacz="";
 				for j in range (1,len(list)):
 					dolacz=dolacz+" "+list[j];
 				obiekt.cardlist.append(dolacz);
-				obiekt.actionlist.append(line);
+				
 		elif("HP_"+obiekt.nick1 in list[0]):
 			nick1hp = list[1];
 		elif ("HP_"+obiekt.nick2 in list[0]):
 			nick2hp=list[1];
 		#samo play, z targetem lub 1/1 cokolwiek to znaczy
 		elif ("play:" in list[0]):
-			continue;
-		
-		else:
-			obiekt.actionlist.append(line);
+			if("mana" in list[1]):
+				slowo=list[1];
+				if (slowo[len(slowo)-1]=="\n"):
+					slowo=slowo[:-1];
+					obiekt.manalist.append(slowo);
+				else:
+					obiekt.manalist.append(list[1]);
+			elif ("target" in line):
+				ind = list.index("target:");
+				dolacz="";
+				for j in range (1,ind):
+					dolacz=dolacz+" "+list[j];
+				if (dolacz[len(dolacz)-1]==",") or (dolacz[len(dolacz)-1] == "\n"):
+					dolacz=dolacz[:-1];
+				obiekt.cardlist.append(dolacz);
+			else:
+				dolacz="";
+				for j in range (1,len(list)):
+					dolacz=dolacz+" "+list[j];
+				if (dolacz[len(dolacz)-1]==",") or (dolacz[len(dolacz)-1] == "\n"):
+					dolacz=dolacz[:-1];			
+					obiekt.cardlist.append(dolacz);
+
 	obiekt.count = obiekt.count + 1;
 #zamykamy plik
 fo.close();
 
-#nie dzialaja nazwy kart i targety
+for k in range (0,len(obiekt.cardlist)):
+	if (obiekt.cardlist[k][len(obiekt.cardlist[k])-1] == "\n"):
+		obiekt.cardlist[k]=obiekt.cardlist[k][:-1];
+	obiekt.cardlist[k]=obiekt.cardlist[k][1:];		
+	if (obiekt.cardlist[k][0]==" "):
+		obiekt.cardlist[k]=obiekt.cardlist[k][1:];
+
 for k in range (0,len(obiekt.cardlist)):
 	print(obiekt.cardlist[k]);
+	
+for k in range (0,len(obiekt.manalist)):
+	print(obiekt.manalist[k]);
 
-#nicki dzialaja
-#hp dzialaja
-#mana dziala
 
-#play, play target i block 
 	
