cc9d9da0ebb446bb92bf841b8dcfe352b0119832
madzer34 <madzer34@gmail.com>
2014-04-26 15:10:00 +0200

    Kolejne funckjonalnosci + poprawki

--- stachecki/stangry2/views.py
+++ stachecki/stangry2/views.py
@@ -129,12 +129,18 @@ def video(request):
 		stats1={};
 		#staty drugie
 		stats2={};
-
-		
+		#klucz to id , value to karta
+		idkarta={};
+		#w co celuje karta
+		celkarty={};
+		#czary
+		czary={};
 		#HP poczatkowe 20
+		
 		#otwieramy plik i tworzymy obiekt gry
-	fo=open('C:\\Users\\np550\\Desktop\\atm\\magic\\history\\LOGI_V3.txt', 'r');
+	fo=open('C:\\Users\\np550\\Desktop\\atm\\magic\\history\\LOGI.txt', 'r');
 	obiekt = Game();
+	wynik = 0;
 
 	#pierwsza petla - czytamy nicki
 	for x in range (0, 1):
@@ -155,14 +161,12 @@ def video(request):
 	#wczytujemy przebieg gry
 	for y in range (1,ogr):
 		line = fo.readline();
-		#gdy linijka odstepu to nie wczytujemy
-		if line == "NEXTTURN":
-			continue;
-		elif not line:break;
+		if not line:break;
 		else:
 		#dzielimy funkcja split dana linie
 			list=line.split (" ");
 			obiekt.count=obiekt.count+1;
+			wynik=0;
 		#wczytujemy akcje
 		if ("PLAY" in list[0]):
 			if (list[2] != "-1" ):
@@ -171,35 +175,117 @@ def video(request):
 					dolacz=list[3];			
 					if (dolacz[len(dolacz)-1]=="\n" or dolacz[len(dolacz)-1]==" "):
 						dolacz=dolacz[:-1];
-					obiekt.cardlist.update({obiekt.count : dolacz});
-					obiekt.idlist.update({obiekt.count : list[2]});
-					obiekt.ktogra.update({obiekt.count : list[1]});
+					fn=open('C:\\Users\\np550\\Desktop\\atm\\magic\\history\\czary.txt', 'r');
+					for k in range (0,70):
+						line2=fn.readline();
+						if not line2 :break;
+						wyraz = line2[:-1];
+						if (dolacz==wyraz):
+							obiekt.czary.update({obiekt.count:dolacz});
+							wynik = 1;
+							break;
+					if (wynik == 0):
+						fl=open('C:\\Users\\np550\\Desktop\\atm\\magic\\history\\stats.txt', 'r');
+						for m in range (0, 100):
+							line3=fl.readline();
+							if not line3 : break;
+							list3 = line3.split(" ");
+							if (dolacz == list3[0]):
+								obiekt.stats1.update({obiekt.count : list3[1]});
+								obiekt.stats2.update({obiekt.count : list3[2][:-1]});
+								break;
+						obiekt.cardlist.update({obiekt.count : dolacz});
+						obiekt.idlist.update({obiekt.count : list[2]});					
+						ind = int(list[2]);
+						obiekt.idkarta.update({ind : dolacz});
+					obiekt.ktogra.update({obiekt.count : list[1]});	
 					if (list[1]=="1"):					
-						obiekt.actionlist.update({obiekt.count : obiekt.nick1+" gra karte "+dolacz+" z id: "+list[2]});
+						obiekt.actionlist.update({obiekt.count : obiekt.nick1+" gra karte "+dolacz});
 					else:
-						obiekt.actionlist.update({obiekt.count : obiekt.nick2+" gra karte "+dolacz+" z id: "+list[2]});
+						obiekt.actionlist.update({obiekt.count : obiekt.nick2+" gra karte "+dolacz});
 					ostktogra = obiekt.count;
 				else:
 					dolacz=list[3];	
 					if (dolacz[len(dolacz)-1]=="\n" or dolacz[len(dolacz)-1]==" "):
 						dolacz=dolacz[:-1];
-					obiekt.cardlist.update({obiekt.count : dolacz});
-					obiekt.idlist.update({obiekt.count : list[2]});
+					fn=open('C:\\Users\\np550\\Desktop\\atm\\magic\\history\\czary.txt', 'r');
+					for k in range (0,70):
+						line2=fn.readline();
+						if not line2 :break;
+						wyraz = line2[:-1];
+						if (dolacz==wyraz):
+							obiekt.czary.update({obiekt.count:dolacz});
+							wynik = 1;
+							break;
+					if (wynik == 0):
+						fl=open('C:\\Users\\np550\\Desktop\\atm\\magic\\history\\stats.txt', 'r');
+						for m in range (0, 100):
+							line3=fl.readline();
+							if not line3 : break;
+							list3 = line3.split(" ");
+							if (dolacz == list3[0]):
+								obiekt.stats1.update({obiekt.count : list3[1]});
+								obiekt.stats2.update({obiekt.count : list3[2][:-1]});
+								break;
+						obiekt.cardlist.update({obiekt.count : dolacz});
+						obiekt.idlist.update({obiekt.count : list[2]});
+						ind = int(list[2]);
+						obiekt.idkarta.update({ind : dolacz});
 					obiekt.ktogra.update({obiekt.count : list[1]});
 					if (list[1]=="1"):					
-						obiekt.actionlist.update({obiekt.count : obiekt.nick1+" "+list[0]+" "+dolacz});
+						obiekt.actionlist.update({obiekt.count : obiekt.nick1+" gra karte "+dolacz});
 					else:
-						obiekt.actionlist.update({obiekt.count : obiekt.nick2+" "+list[0]+" "+dolacz});
+						obiekt.actionlist.update({obiekt.count : obiekt.nick2+" gra karte "+dolacz});
 					ostktogra = obiekt.count;
+			else:
+				dolacz=list[3];	
+				if (dolacz[len(dolacz)-1]=="\n" or dolacz[len(dolacz)-1]==" "):
+					dolacz=dolacz[:-1];
+				fn=open('C:\\Users\\np550\\Desktop\\atm\\magic\\history\\czary.txt', 'r');
+				for k in range (0,70):
+					line2=fn.readline();
+					if not line2 :break;
+					wyraz = line2[:-1];
+					if (dolacz==wyraz):
+						obiekt.czary.update({obiekt.count:dolacz});
+						wynik = 1;
+						break;
+				if (wynik == 0):
+					fl=open('C:\\Users\\np550\\Desktop\\atm\\magic\\history\\stats.txt', 'r');
+					for m in range (0, 100):
+						line3=fl.readline();
+						if not line3 : break;
+						list3 = line3.split(" ");
+						if (dolacz == list3[0]):
+							obiekt.stats1.update({obiekt.count : list3[1]});
+							obiekt.stats2.update({obiekt.count : list3[2][:-1]});
+							break;
+					obiekt.cardlist.update({obiekt.count : dolacz});
+					obiekt.idlist.update({obiekt.count : list[2]});
+					ind = int(list[2]);
+					obiekt.idkarta.update({ind : dolacz});
+				obiekt.ktogra.update({obiekt.count : list[1]});
+				if (list[1]=="1"):					
+					obiekt.actionlist.update({obiekt.count : obiekt.nick1+" gra karte "+dolacz});
+				else:
+					obiekt.actionlist.update({obiekt.count : obiekt.nick2+" gra karte "+dolacz});
+			if (list[4] != "-1" and list[4] != " " and list[4]!="\n" and len(list)>4 ):
+				obiekt.celkarty.update({obiekt.count : list[4]});
 		elif ("SUBLIFE" in list[0]):
 			if ("1" in list[1]):
 				list[2]=list[2][:-1];
 				obiekt.hp1list.update({obiekt.count : list[2]});
-				obiekt.actionlist.update({obiekt.count : "Wskaznik zdrowia gracza "+ obiekt.nick1+": "+list[2]});
+				if (list[2]!="0"):
+					obiekt.actionlist.update({obiekt.count : "Wskaznik zdrowia gracza "+ obiekt.nick1+": "+list[2]});
+				else:
+					obiekt.actionlist.update({obiekt.count : "Gracz "+ obiekt.nick2+" odnosi zwyciestwo!"});
 			else: 
 				list[2]=list[2][:-1];
 				obiekt.hp2list.update({obiekt.count : list[2]});
-				obiekt.actionlist.update({obiekt.count : "Wskaznik zdrowia gracza "+ obiekt.nick2+": "+list[2]});
+				if (list[2]!="0"):
+					obiekt.actionlist.update({obiekt.count : "Wskaznik zdrowia gracza "+ obiekt.nick2+": "+list[2]});
+				else :
+					obiekt.actionlist.update({obiekt.count : "Gracz "+ obiekt.nick1+" odnosi zwyciestwo!"});
 		elif ("NEXTTURN" in list[0]):
 			element = obiekt.ktogra.get(ostktogra,obiekt.ktogra);
 			obiekt.nextturn.update({obiekt.count : element});
@@ -208,24 +294,38 @@ def video(request):
 			obiekt.stats.update({obiekt.count : list[1]});
 			obiekt.stats1.update({obiekt.count : list[2]});
 			obiekt.stats2.update({obiekt.count : list[3]});
-			obiekt.actionlist.update({obiekt.count : "Nadanie statystyki karcie z id: "+list[1]+"  wartosci: "+list[2]+" "+list[3][:-1]});
+			ind=int(list[1]);
+			element=obiekt.idkarta[ind];
+			obiekt.actionlist.update({obiekt.count : "Nadanie statystyki karcie: "+element+"  wartosci: "+list[2]+" "+list[3][:-1]});
 		elif ("DEFENCE" in list[0]):
 			obiekt.defenslist.update({obiekt.count : list[2][:-1]});
 			obiekt.celeobrony.update({obiekt.count : list[1]});
-			obiekt.actionlist.update({obiekt.count : "Karta z id: "+list[2][:-1]+" broni sie!"});
+			ind=int(list[2][:-1]);
+			element=obiekt.idkarta[ind];
+			ind2 = int(list[1]);
+			element2=obiekt.idkarta[ind2];
+			obiekt.actionlist.update({obiekt.count : "Karta "+element+" broni atak karty "+element2+"!"});
 		elif ("ATTACK" in list[0]):
 			obiekt.ataklist.update({obiekt.count : list[1][:-1]});
-			obiekt.actionlist.update({obiekt.count : "Atak karty z id: "+list[1][:-1]});
+			ind=int(list[1]);
+			element=obiekt.idkarta[ind];
+			obiekt.actionlist.update({obiekt.count : "Atak karty "+element+"!"});
 		elif ("DEAD" in list[0]):
 			obiekt.deadlist.update({obiekt.count : list[1][:-1]});
-			obiekt.actionlist.update({obiekt.count : "Smierc karty z id: "+list[1][:-1]});
+			ind=int(list[1][:-1]);
+			element=obiekt.idkarta[ind];
+			obiekt.actionlist.update({obiekt.count : "Smierc karty "+element});
 		elif ("ADDDAMAGE" in list[0]):
-			obiekt.dmglist.update({obiekt.count : list [1]});
+			obiekt.dmglist.update({obiekt.count : list[1]});
 			obiekt.dmgvalue.update({obiekt.count : list[2]});
-			obiekt.actionlist.update({obiekt.count : "Zmiana wartosci damage dla karty z id: "+list[1]+" na wartosc: "+list[2][:-1]});
+			ind=int(list[1]);
+			element=obiekt.idkarta[ind];
+			obiekt.actionlist.update({obiekt.count : "Zmiana wartosci damage dla karty  "+element+" na wartosc: "+list[2][:-1]});
 		#zamykamy plik
 	fo.close();
-	return render_to_response ('video.html',{'karty':obiekt.cardlist, 'licznik':obiekt.count, 'gracz1': 1, 'gracz2': 2, 'ktogra':obiekt.ktogra, 'akcje':obiekt.actionlist, 'deads':obiekt.deadlist, 'idlist':obiekt.idlist, 'ileakcji':obiekt.count, 'hp1':obiekt.hp1list, 'hp2':obiekt.hp2list, 'ataki':obiekt.ataklist,'obrony':obiekt.defenslist, 'dmg':obiekt.dmglist,'dmgvalue':obiekt.dmgvalue, 'tury':obiekt.nextturn, 'cele':obiekt.celeobrony, 'stats':obiekt.stats, 'stats1':obiekt.stats1, 'stats2':obiekt.stats2})
+	fn.close();
+	fl.close();
+	return render_to_response ('video.html',{'karty':obiekt.cardlist, 'licznik':obiekt.count, 'gracz1': 1, 'gracz2': 2, 'ktogra':obiekt.ktogra, 'akcje':obiekt.actionlist, 'deads':obiekt.deadlist, 'idlist':obiekt.idlist, 'ileakcji':obiekt.count, 'hp1':obiekt.hp1list, 'hp2':obiekt.hp2list, 'ataki':obiekt.ataklist,'obrony':obiekt.defenslist, 'dmg':obiekt.dmglist,'dmgvalue':obiekt.dmgvalue, 'tury':obiekt.nextturn, 'cele':obiekt.celeobrony, 'stats':obiekt.stats, 'stats1':obiekt.stats1, 'stats2':obiekt.stats2, 'celataku':obiekt.celkarty, 'czary':obiekt.czary})
 
 def popular(request):
     
