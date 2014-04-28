ea95f646da7e305fd5c4eebb6a1d8db52502ac62
madzer34 <madzer34@gmail.com>
2014-04-28 21:41:29 +0200

    Linie ataku, statsy, rozpoznawanie czarow

--- stachecki/stangry2/views.py
+++ stachecki/stangry2/views.py
@@ -129,6 +129,8 @@ def video(request):
 		stats1={};
 		#staty drugie
 		stats2={};
+		#jak dlugo beda statsy utrzymywac sie
+		jakdlugo={};
 		#klucz to id , value to karta
 		idkarta={};
 		#w co celuje karta
@@ -191,6 +193,7 @@ def video(request):
 							if not line3 : break;
 							list3 = line3.split(" ");
 							if (dolacz == list3[0]):
+								obiekt.stats.update({obiekt.count : list[2]});
 								obiekt.stats1.update({obiekt.count : list3[1]});
 								obiekt.stats2.update({obiekt.count : list3[2][:-1]});
 								break;
@@ -224,6 +227,7 @@ def video(request):
 							if not line3 : break;
 							list3 = line3.split(" ");
 							if (dolacz == list3[0]):
+								obiekt.stats.update({obiekt.count : list[2]});
 								obiekt.stats1.update({obiekt.count : list3[1]});
 								obiekt.stats2.update({obiekt.count : list3[2][:-1]});
 								break;
@@ -257,6 +261,7 @@ def video(request):
 						if not line3 : break;
 						list3 = line3.split(" ");
 						if (dolacz == list3[0]):
+							obiekt.stats.update({obiekt.count : list[2]});
 							obiekt.stats1.update({obiekt.count : list3[1]});
 							obiekt.stats2.update({obiekt.count : list3[2][:-1]});
 							break;
@@ -273,14 +278,16 @@ def video(request):
 				obiekt.celkarty.update({obiekt.count : list[4]});
 		elif ("SUBLIFE" in list[0]):
 			if ("1" in list[1]):
-				list[2]=list[2][:-1];
+				if (list[2][len(list[2])-1]=="\n"):
+					list[2]=list[2][:-1];
 				obiekt.hp1list.update({obiekt.count : list[2]});
 				if (list[2]!="0"):
 					obiekt.actionlist.update({obiekt.count : "Wskaznik zdrowia gracza "+ obiekt.nick1+": "+list[2]});
 				else:
 					obiekt.actionlist.update({obiekt.count : "Gracz "+ obiekt.nick2+" odnosi zwyciestwo!"});
 			else: 
-				list[2]=list[2][:-1];
+				if (list[2][len(list[2])-1]=="\n"):
+					list[2]=list[2][:-1];
 				obiekt.hp2list.update({obiekt.count : list[2]});
 				if (list[2]!="0"):
 					obiekt.actionlist.update({obiekt.count : "Wskaznik zdrowia gracza "+ obiekt.nick2+": "+list[2]});
@@ -294,12 +301,13 @@ def video(request):
 			obiekt.stats.update({obiekt.count : list[1]});
 			obiekt.stats1.update({obiekt.count : list[2]});
 			obiekt.stats2.update({obiekt.count : list[3]});
+			#obiekt.jakdlugo.update({obiekt.count :list[4]});
 			ind=int(list[1]);
 			element=obiekt.idkarta[ind];
 			obiekt.actionlist.update({obiekt.count : "Nadanie statystyki karcie: "+element+"  wartosci: "+list[2]+" "+list[3][:-1]});
 		elif ("DEFENCE" in list[0]):
 			obiekt.defenslist.update({obiekt.count : list[2][:-1]});
-			obiekt.celeobrony.update({obiekt.count : list[1]});
+			#obiekt.celeobrony.update({obiekt.count : list[1]});
 			ind=int(list[2][:-1]);
 			element=obiekt.idkarta[ind];
 			ind2 = int(list[1]);
@@ -325,7 +333,7 @@ def video(request):
 	fo.close();
 	fn.close();
 	fl.close();
-	return render_to_response ('video.html',{'karty':obiekt.cardlist, 'licznik':obiekt.count, 'gracz1': 1, 'gracz2': 2, 'ktogra':obiekt.ktogra, 'akcje':obiekt.actionlist, 'deads':obiekt.deadlist, 'idlist':obiekt.idlist, 'ileakcji':obiekt.count, 'hp1':obiekt.hp1list, 'hp2':obiekt.hp2list, 'ataki':obiekt.ataklist,'obrony':obiekt.defenslist, 'dmg':obiekt.dmglist,'dmgvalue':obiekt.dmgvalue, 'tury':obiekt.nextturn, 'cele':obiekt.celeobrony, 'stats':obiekt.stats, 'stats1':obiekt.stats1, 'stats2':obiekt.stats2, 'celataku':obiekt.celkarty, 'czary':obiekt.czary})
+	return render_to_response ('video.html',{'karty':obiekt.cardlist, 'licznik':obiekt.count, 'gracz1': 1, 'gracz2': 2, 'ktogra':obiekt.ktogra, 'akcje':obiekt.actionlist, 'deads':obiekt.deadlist, 'idlist':obiekt.idlist, 'ileakcji':obiekt.count, 'hp1':obiekt.hp1list, 'hp2':obiekt.hp2list, 'ataki':obiekt.ataklist,'obrony':obiekt.defenslist, 'dmg':obiekt.dmglist,'dmgvalue':obiekt.dmgvalue, 'tury':obiekt.nextturn, 'cele':obiekt.celeobrony, 'stats':obiekt.stats, 'stats1':obiekt.stats1, 'stats2':obiekt.stats2, 'celeataku':obiekt.celkarty, 'czary':obiekt.czary})
 
 def popular(request):
     
